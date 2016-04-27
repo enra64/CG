af1edd56b0af25bf1592edd80ab2dafa354f0688
Arne <sujomear@arcor.de>
2016-04-27 22:46:12 +0200

    well that took some time... i have a feeling that vectors in c++ dont operate the way i thought they do

--- Task2/exercises/raytracer/BezierPatchMesh.cpp
+++ Task2/exercises/raytracer/BezierPatchMesh.cpp
@@ -95,20 +95,77 @@ namespace rt
     return bbox;
   }
 
+  /** \brief evaluate bezier curve
+   *
+   * @param cntrl control points for the bezier curve
+   * @param u the position of the evaluation
+   * @param tangent a tangent to the result created by the last two control points.
+   */
+  Vec3d BezierPatchMesh::deCasteljau(const std::vector<Vec3d>& cntrl, double p, Vec3d& tangent) const{
+      // get a copy that we can modify
+      std::vector<Vec3d> tmp(cntrl);
+      // we need the last two points for the tangent
+      while(tmp.size() > 2){
+        // casteljau step
+        for(size_t i = 0; i < tmp.size() - 1; i++)
+          tmp[i] = tmp[i] * (1 - p) + tmp[i + 1] * p;
+        // the last point is now obsolete
+        tmp.resize(tmp.size() - 1);
+      }
+      // calculate tangent
+      tangent = tmp[1] - tmp[0];
+      // do the last step, this is the interpolation result
+      return tmp[0] * (1 - p) + tmp[1] * p;
+  }
 
   BezierPatchMesh::BezierPatchSample BezierPatchMesh::sample(double u, double v) const
   {
     BezierPatchSample ret;
     ret.uv = Vec2d(u,v);
 
-    // Programming TASK 2: implement this method
-    // You need to compute ret.position and ret.normal!
+    // u and v control points
+    std::vector<Vec3d> uPoints(mM), tmp;
+    std::vector<Vec3d> vPoints(mN);
+    
+    // the two tangents
+    Vec3d uTangent;
+    Vec3d vTangent;
 
-    // This data will be used within the initialize() function for the 
-    // triangle mesh construction.
+    // first interpolation run (along u)
+    for(size_t j = 0; j < mN; j++){
+      // get control points for the u axis
+      for(size_t i = 0; i < mM; i++)
+        uPoints[i] = controlPoint(i, j);
 
-    ret.position = Vec3d();
-    ret.normal = Vec3d();
+      // get the point on the bezier curve for this set of control points
+      vPoints[j] = deCasteljau(uPoints, u, vTangent);
+    }
+
+    // get the actual v tangent
+    deCasteljau(vPoints, v, vTangent);
+
+    // multiply by number of control points
+    vTangent *= vPoints.size();
+
+    // second interpolation run (along v)
+    for(size_t i = 0; i < mM; i++){
+      // get control points for the v axis
+      for(size_t j = 0; j < mN; j++)
+        vPoints[j] = controlPoint(i, j); // i==u, j==v
+
+      // get the point on the bezier curve for this set of control points
+      uPoints[i] = deCasteljau(vPoints, v, uTangent);
+    }
+
+    // get the actual u tangent, and the calculated position, as this is the final interpolation run
+    ret.position = deCasteljau(uPoints, u, uTangent);
+
+    // multiply tangent by number of control points
+    uTangent *= uPoints.size();
+
+    // the normal is now the normalized cross product between the two tangents
+    ret.normal = cross(uTangent, vTangent).normalize();
+
     return ret;
   }
 } //namespace rt

6171a7937a5668dd9a4d9b39dd78c569b30de0cb
enra64 <sujomear@arcor.de>
2016-06-19 14:35:41 +0200

    task 4

---
+++ Task4/build/exercises/raytracer_task2/CMakeFiles/raytracer_task2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer_task2.dir/raytracer_task2.cpp.o"
+  "../../raytracer_task2.pdb"
+  "../../raytracer_task2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer_task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task4/build/exercises/raytracer_task3/CMakeFiles/raytracer_task3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/run/media/arne/data/Studium/Semester 4/CG/Task4/exercises/raytracer_task3/raytracer_task3.cpp" "/run/media/arne/data/Studium/Semester 4/CG/Task4/build/exercises/raytracer_task3/CMakeFiles/raytracer_task3.dir/raytracer_task3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../exercises"
+  "../depend/glew-1.10.0/include"
+  "../depend/GLFW-3.0.4/include"
+  "../exercises/raytracer_task3/opengl"
+  "../exercises/raytracer_task3/raytracer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/run/media/arne/data/Studium/Semester 4/CG/Task4/build/exercises/opengl/CMakeFiles/opengl.dir/DependInfo.cmake"
+  "/run/media/arne/data/Studium/Semester 4/CG/Task4/build/exercises/raytracer/CMakeFiles/raytracer.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
